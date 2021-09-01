00580ea59d5143ebf9576c37e45450bbfb41896c
Sandeep Rout <f20201711@goa.bits-pilani.ac.in>
2021-09-01 18:31:05 +0530

    second commit

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:world_time/services/world_time.dart';
+import 'package:world_time/pages/home.dart';
 
 class ChooseLocation extends StatefulWidget {
   @override
@@ -6,6 +8,32 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
+  List<Worldtime> locations = [
+    Worldtime(
+      url: 'Europe/London',
+      location: 'London',
+      flag: 'uk.png',
+    ),
+    Worldtime(url: 'Europe/Berlin', location: 'Berlin', flag: 'germany.png'),
+    // Worldtime(url: 'Africa/Cairo', location: 'Cairo', flag: 'egypt.png'),
+    Worldtime(url: 'Africa/Nairobi', location: 'Nairobi', flag: 'kenya.png'),
+    Worldtime(url: 'America/Chicago', location: 'Chicago', flag: 'usa.png'),
+    Worldtime(url: 'America/New_York', location: 'New York', flag: 'usa.png'),
+    Worldtime(url: 'Asia/Seoul', location: 'Seoul', flag: 'south_korea.png'),
+    Worldtime(url: 'Asia/Jakarta', location: 'Jakarta', flag: 'indonesia.png'),
+  ];
+  // ignore: non_constant_identifier_names
+  void Updatetime(index) async {
+    Worldtime instance = locations[index];
+    await instance.gettime();
+    Navigator.pop(context, {
+      'location': locations[index].location,
+      'flag': locations[index].flag,
+      'time': locations[index].time,
+      'isDaytime': locations[index].isDaytime,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     print('build function ran');
@@ -17,8 +45,25 @@ class _ChooseLocationState extends State<ChooseLocation> {
         centerTitle: true,
         elevation: 0,
       ),
-      body: RaisedButton(
-        onPressed: () {},
+      body: ListView.builder(
+        itemCount: locations.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 1.0, horizontal: 4.0),
+            child: Card(
+              child: ListTile(
+                onTap: () {
+                  Updatetime(index);
+                },
+                title: Text(locations[index].location),
+                leading: CircleAvatar(
+                  backgroundImage:
+                      AssetImage('lib/assets/${locations[index].flag}'),
+                ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
